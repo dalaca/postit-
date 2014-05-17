1fc6c9d6acba9897a3ac67473ce61ba10fa8262e
David <david@David.(none)>
2014-05-16 20:09:20 -0400

    completed post show and index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
   def index
-
+  	@post = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+  
 end
