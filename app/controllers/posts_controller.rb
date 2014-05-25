fd6b30f2cdc502b50524a8599f86e3df1e228751
David <david@David.(none)>
2014-05-25 19:17:39 -0400

    Added Form Partials and post views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,46 @@
 class PostsController < ApplicationController
+  before_action :set_post, only:[:show, :edit, :update]
   def index
   	@post = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
+    
+  end
+
+  def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Your Post was saved"
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
+    
   end
 
   def update
+    
+    if @post.update(post_params)
+      flash[:notice] = "Your Post was updated"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
-  
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ has_many :comments
 belongs_to :user
 has_many :post_categories
 has_many :categories, through: :post_categories
+
+validates :title, :url, presence: true
 end
